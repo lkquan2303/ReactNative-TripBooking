a7bb1cf52de59e9a07e8784917292fcc5d3c051b
Quan (Uri) K.LE <uri@enclave.vn>
2021-02-08 17:18:03 +0700

    Bug Component in Home Screens

--- App.js
+++ App.js
@@ -1,2 +1,21 @@
 import React from 'react';
-import {TouchableOpacity, Image} from 'react-native';
+
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import OnboardingScreen from './screens/Onboarding';
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Onboarding">
+        <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default MyStack;
